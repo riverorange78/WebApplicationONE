889dee3eb2efa2e113bc62d67d1e94b2e8c38f9c
riverorange <riverorange@yandex.ru>
2021-05-01 12:17:50 +0300

    333

---
+++ Hj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationONE
+{
+    [Serializable]
+	class Hj
+	{
+		public string videoId { get; set; }
+		public string  description { get; set; }
+	}
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Blazored.LocalStorage;
 
 namespace WebApplicationONE
 {
@@ -17,8 +18,22 @@ namespace WebApplicationONE
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(
+                sp => new HttpClient
+                {
+                    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+                })
+                .AddBlazoredLocalStorage(config =>
+        config.JsonSerializerOptions.WriteIndented = true);
 
+            //builder.Services.AddBlazoredLocalStorage();
+            //------------
+            //builder.Services.AddTransient(sp => new HttpClient
+            //{
+            //    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            //})
+            //    .AddBlazoredLocalStorage();
+            //------------
             await builder.Build().RunAsync();
         }
     }
